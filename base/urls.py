3cc8d7d8f55b969e1a9db77786b019d0b94905da
dev-langesh <dev.langesh@gmail.com>
2022-09-30 12:43:50 +0530

    create todo functionality

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.createTodo,name='home')
+    path('',views.getTodos,name='home'),
+    path('create/',views.createTodo,name='create')
 ]
--- base/views.py
+++ base/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from .forms import TodoForm
+
 # Create your views here.
 
 from .models import Todo
 
-def createTodo(request):
+def getTodos(request):
 
     data = Todo.objects.all()
 
     context = {'todos' : data}
 
     return render(request,'base/home.html',context)
+
+def createTodo(request):
+
+    form = TodoForm()
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+
+    context = {'form' : form}
+
+    return render(request,'base/create.html',context)
