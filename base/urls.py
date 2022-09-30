f0712fabc696ad9dbdebb40e48dac127714d24fd
dev-langesh <dev.langesh@gmail.com>
2022-09-30 12:32:54 +0530

    all todo

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/',views.createTodo,name='create')
+    path('',views.createTodo,name='home')
 ]
--- base/views.py
+++ base/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import Todo
+
 def createTodo(request):
-    return HttpResponse("create")
+
+    data = Todo.objects.all()
+
+    context = {'todos' : data}
+
+    return render(request,'base/home.html',context)
